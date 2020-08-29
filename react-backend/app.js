a9ea87a21224d4382a9377602e28e8ad98f0a6a1
RheshavMaharaj <rheshav@hotmail.com>
2020-08-29 18:58:35 +1000

    Adding Database Changes

--- react-backend/app.js
+++ react-backend/app.js
@@ -1,3 +1,5 @@
+//Server.js
+
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
@@ -8,6 +10,81 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+
+/////////////////////////////////////////////////////
+
+const {MongoClient} = require('mongodb');
+
+async function main(){
+  /**
+   * Connection URI. Update <username>, <password>, and <your-cluster-url> to reflect your cluster.
+   * See https://docs.mongodb.com/ecosystem/drivers/node/ for more details
+   */
+  const uri = "mongodb+srv://Rheshav:SBgxypqdhUv859Q@sesg3.8gnmg.azure.mongodb.net/<dbname>?retryWrites=true&w=majority";
+
+
+  const client = new MongoClient(uri);
+
+  try {
+      // Connect to the MongoDB cluster
+      await client.connect();
+
+      // Make the appropriate DB calls
+      await  listDatabases(client);
+
+  } catch (e) {
+      console.error(e);
+  } finally {
+      await client.close();
+  }
+}
+
+main().catch(console.error);
+
+async function listDatabases(client){
+  databasesList = await client.db().admin().listDatabases();
+
+  console.log("Databases:");
+  databasesList.databases.forEach(db => console.log(` - ${db.name}`));
+
+  const db = client.db("eLibrary");
+  /*
+  var cursor = db.collection('Users').insertOne({
+    item: "canvas",
+    qty: 100,
+    tags: ["cotton"],
+    size: { h: 28, w: 35.5, uom: "cm" }
+  })
+  .then(function(result) {
+    // process result
+  })
+  */
+  db.collection('Users').deleteMany({ 
+    tags: "cotton"
+  })
+  .then(function(result) {
+    // process result
+  })
+
+  var cursor = db.collection('Users').find({});
+
+  function iterateFunc(doc) {
+    console.log(JSON.stringify(doc, null, 4));
+  }
+
+  function errorFunc(error) {
+    console.log(error);
+  }
+
+  cursor.forEach(iterateFunc, errorFunc);
+
+};
+
+
+
+
+////////////////////////////////////////////////////////////
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
