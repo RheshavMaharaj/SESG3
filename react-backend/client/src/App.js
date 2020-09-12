82e68444dacb4a6d00406b362c98c95302b4b1ca
Ayush Kanwal <53325312+ayushkanwal@users.noreply.github.com>
2020-09-12 14:39:17 +0530

    added notif. pane and navbar changes

--- react-backend/client/src/App.js
+++ react-backend/client/src/App.js
@@ -14,10 +14,13 @@ import './App.css';
 import about from './Assets/help.svg';
 import home from './Assets/home-run.svg';
 import user from './Assets/user.svg';
+import notification from './Assets/notification.svg';
+
 
 import Home from './Home.js';
 import About from './About.js';
 import User from './User.js';
+
 
 
 
@@ -28,13 +31,26 @@ export default function App() {
       <div>
         <div className="Toolbar">
           <img src={logo} className="App-logo" alt="logo" />
-          eLibrary Suite
-          <nav>
+         <h1>eLibrary Suite</h1> 
+           <nav>
+
+             <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                 Notification
+                 <span class="badge badge-light">3</span> 
+              </button>
+               <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                  <a class="dropdown-item" href="#">Action</a>
+                  <a class="dropdown-item" href="#">Another action</a>
+                  <a class="dropdown-item" href="#">Something else here</a>
+                </div>
+           
             <Link to="/"><img src={home} className="App-logo" alt="home" /></Link>
             <Link to="/about"><img src={about} className="App-logo" alt="about" /></Link>
             <Link to="/users"><img src={user} className="App-logo" alt="user" /></Link>
           </nav>
         </div>
+       <div>
+       </div>
         <div>
           <Switch>
             <Route path="/about">
@@ -49,7 +65,6 @@ export default function App() {
           </Switch>
         </div>
       </div>
-    </Router>
-    
+    </Router>      
   );
 }
