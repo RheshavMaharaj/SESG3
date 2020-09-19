7bf997a4f61c3a719a5039e84c60bf6fb2755ef2
RheshavMaharaj <rheshav@hotmail.com>
2020-09-20 01:34:47 +1000

    Added Jamane Marcelino changes on about.js

--- react-backend/client/src/About.js
+++ react-backend/client/src/About.js
@@ -3,28 +3,17 @@ import React, {Component} from "react";
 export default class About extends Component {
   render(){
     return(
-    <div>
-      {/* Highlight List View imported from bootstrap example code. May be removed */}
-      <h1>This is the About screen</h1>
-      <div class="row">
-        <div class="col-4">
-          <div class="list-group" id="list-tab" role="tablist">
-            <a class="list-group-item list-group-item-action active" id="list-home-list" data-toggle="list" href="#list-home" role="tab" aria-controls="home">Home</a>
-            <a class="list-group-item list-group-item-action" id="list-profile-list" data-toggle="list" href="#list-profile" role="tab" aria-controls="profile">Profile</a>
-            <a class="list-group-item list-group-item-action" id="list-messages-list" data-toggle="list" href="#list-messages" role="tab" aria-controls="messages">Messages</a>
-            <a class="list-group-item list-group-item-action" id="list-settings-list" data-toggle="list" href="#list-settings" role="tab" aria-controls="settings">Settings</a>
-          </div>
-        </div>
-        <div class="col-8">
-          <div class="tab-content" id="nav-tabContent">
-            <div class="tab-pane fade show active" id="list-home" role="tabpanel" aria-labelledby="list-home-list">...</div>
-            <div class="tab-pane fade" id="list-profile" role="tabpanel" aria-labelledby="list-profile-list">...</div>
-            <div class="tab-pane fade" id="list-messages" role="tabpanel" aria-labelledby="list-messages-list">...</div>
-            <div class="tab-pane fade" id="list-settings" role="tabpanel" aria-labelledby="list-settings-list">...</div>
-          </div>
-        </div>
+      <div class="container">
+        <h1>About</h1>
+        <h4>What is this website?</h4>
+        <h4>Contact Information</h4>
+        <b>Email: </b> sesg3@uts.com (change this to whatever)
+        <div><b>Phone: </b> 04198237198212315 </div>
+        <b>Additional Contacts: </b>
+        <h4>Who Made it?</h4>
+        <t3>Team Tree (or SESG3) comprises of 7 members, and built this thing as a project for uni etc etc etc</t3>
+        <div><img src={logo} width="200" height="200"  /></div>
       </div>
-    </div>
     )
   }
 }
