acb61811c5f96e6b88d67f8dbaec324e67992176
Ayush Kanwal <53325312+ayushkanwal@users.noreply.github.com>
2020-09-22 15:22:53 +0530

    added a basic layout for the user account front end

--- react-backend/client/src/Home.js
+++ react-backend/client/src/Home.js
@@ -1,89 +1,143 @@
 //Imports
-import React, {Component} from "react";
-import formula1 from './Assets/f1.png';
-import formula2 from './Assets/f2.png';
-import sunset from './Assets/sunset.png';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './Home.css';
+import React, { Component } from "react";
+import formula1 from "./Assets/f1.png";
+import formula2 from "./Assets/f2.png";
+import sunset from "./Assets/sunset.png";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./Home.css";
 
 //Class and subsequent functions
 class Home extends Component {
-  state = {books: [], test: ''}
+  state = { books: [], test: "" };
 
   /* function to retrieve documents from mongodb database collection. Runs on every page reload */
   componentDidMount() {
-    fetch('/get-data')
-      .then(res => res.json())
-      .then(books => this.setState({ books }));
-    fetch('/test')
-      .then(res => res.json())
-      .then(test => this.setState({ test }));
+    fetch("/get-data")
+      .then((res) => res.json())
+      .then((books) => this.setState({ books }));
+    fetch("/test")
+      .then((res) => res.json())
+      .then((test) => this.setState({ test }));
   }
 
   render() {
     return (
       <div>
         {/* Highlight Carousel imported from bootstrap example code. May be removed */}
-        <div id="carouselExampleControls" className="carousel slide" data-ride="carousel">
+        <div
+          id="carouselExampleControls"
+          className="carousel slide"
+          data-ride="carousel"
+        >
           <div className="carousel-inner">
             <div className="carousel-item active">
-              <img className="d-block w-100" src={formula1} alt="First slide"/>
+              <img className="d-block w-100" src={formula1} alt="First slide" />
             </div>
             <div className="carousel-item">
-              <img className="d-block w-100" src={formula2} alt="Second slide"/>
+              <img
+                className="d-block w-100"
+                src={formula2}
+                alt="Second slide"
+              />
             </div>
             <div className="carousel-item">
-              <img className="d-block w-100" src={sunset}alt="Third slide"/>
+              <img className="d-block w-100" src={sunset} alt="Third slide" />
             </div>
           </div>
-          <a className="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
-            <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+          <a
+            className="carousel-control-prev"
+            href="#carouselExampleControls"
+            role="button"
+            data-slide="prev"
+          >
+            <span
+              className="carousel-control-prev-icon"
+              aria-hidden="true"
+            ></span>
             <span className="sr-only">Previous</span>
           </a>
-          <a className="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
-            <span className="carousel-control-next-icon" aria-hidden="true"></span>
+          <a
+            className="carousel-control-next"
+            href="#carouselExampleControls"
+            role="button"
+            data-slide="next"
+          >
+            <span
+              className="carousel-control-next-icon"
+              aria-hidden="true"
+            ></span>
             <span className="sr-only">Next</span>
           </a>
         </div>
-        {
-        /*
+        {/*
         //Get data link to force app to show new updates to the database on screen - use for light testing purposes only
         <div>
           <a href="/get-data">Load Data</a>
         </div>
-        */
-        }
+        */}
         <div className="list-group">
           {/* Loop to display the resource data into a bootstrap wrapped element. */}
           <h3>Your Resources</h3>
-            {this.state.books.map(book =>
-              <div key={book._id}>
-                <button type="button" className="list-group-item list-group-item-action"> {book.title} </button>
-                {/* || Description: {user.content} || Developer: {user.author} */}
-              </div>
-            )}
+          {this.state.books.map((book) => (
+            <div key={book._id}>
+              <button
+                type="button"
+                className="list-group-item list-group-item-action"
+              >
+                {" "}
+                {book.title}{" "}
+              </button>
+              {/* || Description: {user.content} || Developer: {user.author} */}
+            </div>
+          ))}
         </div>
         {/* Form to insert documents into the mongodb database collection */}
         <div>
-          <form action="/insert" method="post" className = "insert-form">
+          <form action="/insert" method="post" className="insert-form">
             <h3>Add Resource to Catalogue</h3>
             <div class="form-group">
               <label for="Title">Resource Title</label>
-              <input type="text" class="form-control" id="title" name="title" placeholder="Enter title"/>
+              <input
+                type="text"
+                class="form-control"
+                id="title"
+                name="title"
+                placeholder="Enter title"
+              />
             </div>
             <div class="form-group">
               <label for="Description">Description</label>
-              <input type="text" class="form-control" id="description" name="description" placeholder="Enter Resource Description"/>
+              <input
+                type="text"
+                class="form-control"
+                id="description"
+                name="description"
+                placeholder="Enter Resource Description"
+              />
             </div>
             <div class="form-group">
               <label for="Author">Author</label>
-              <input type="text" class="form-control" id="author" name="author" placeholder="Enter Resource Author(s)"/>
+              <input
+                type="text"
+                class="form-control"
+                id="author"
+                name="author"
+                placeholder="Enter Resource Author(s)"
+              />
             </div>
             <div class="form-group">
               <label for="Reference Number">Reference Number</label>
-              <input type="number" class="form-control" id="refnumber" name="refnumber" placeholder="Enter Resource Reference Number"/>
+              <input
+                type="number"
+                class="form-control"
+                id="refnumber"
+                name="refnumber"
+                placeholder="Enter Resource Reference Number"
+              />
             </div>
-            <button type="submit" class="btn btn-primary">Submit</button>
+            <button type="submit" class="btn btn-primary">
+              Submit
+            </button>
           </form>
           {this.state.test}
         </div>
@@ -93,4 +147,3 @@ class Home extends Component {
 }
 
 export default Home;
-
--- react-backend/client/src/User.js
+++ react-backend/client/src/User.js
@@ -1,10 +1,77 @@
 import React, { Component } from "react";
 
-export default class User extends Component{
-  
-  render(){
-    return(
-      <h1>This is the User screen</h1>
-    )
+import image1 from "./Assets/userplaceholder.png";
+
+export default class User extends Component {
+  render() {
+    return (
+      <div>
+        <div class="container-fluid ">
+          <h1 align="center">This is the user account screen</h1>
+          <p align="center">
+            Here user can view/edit their personal information and review books
+            borrowed and history of brrowed books
+          </p>
+          <div class="row">
+            <div class="col-8 bg-dark text-white">
+              <img
+                src={image1}
+                alt="User photo"
+                class="mr-5 mt-4 rounded-circle"
+              ></img>
+
+              <form>
+                <div class="row">
+                  <div class="col">
+                    <label for="staticEmail" class="col-sm-2">
+                      Email
+                    </label>
+                  </div>
+                  <div class="col">
+                    <label for="staticEmail" class="col-sm-2">
+                      Email
+                    </label>
+                  </div>
+                </div>
+
+                <div class="row">
+                  <label for="staticEmail" class="col-sm-2">
+                    Email
+                  </label>
+                  <div class="col">
+                    <label for="staticEmail" class="col-sm-2">
+                      Email
+                    </label>
+                  </div>
+                </div>
+                <div class="row">
+                  <label for="staticEmail" class="col-sm-2">
+                    Email
+                  </label>
+                  <div class="col">
+                    <label for="staticEmail" class="col-sm-2">
+                      Email
+                    </label>
+                  </div>
+                </div>
+
+                <div class="row">
+                  <label for="staticEmail" class="col-sm-2">
+                    Email
+                  </label>
+                  <div class="col">
+                    <label for="staticEmail" class="col-sm-2">
+                      Email
+                    </label>
+                  </div>
+                </div>
+              </form>
+            </div>
+
+            <div class="col-4 bg-primary text-white">book information</div>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
